0213d5a8201df34c493e741bb636cf93bbc0daf2
Vivek <vivek.a.sivakumar@gmail.com>
2016-10-27 17:47:12 -0700

    multiple lists

--- lab2b/lab2_list.c
+++ lab2b/lab2_list.c
@@ -8,9 +8,16 @@
 
 #define KEY_LENGTH 5
 
-pthread_mutex_t mutex;
+typedef enum sync {NONE, MUTEX, SPIN} sync_option;
 
-volatile int lock = 0;
+typedef struct locks {
+	pthread_mutex_t mutex;
+	volatile int spin_lock;
+} listLocks;
+
+// pthread_mutex_t mutex;
+
+// volatile int lock = 0;
 
 int opt_yield = 0;
 
@@ -19,69 +26,137 @@ void error(char *msg){
 	exit(1);
 }
 
-void no_op(){}
-
-void mutex_lock(){
-	pthread_mutex_lock(&mutex);
-}
-
-void mutex_unlock(){
-	pthread_mutex_unlock(&mutex);
-}
-
-void spin_lock(){
-	while(__sync_lock_test_and_set(&lock, 1));
-}
-
-void spin_unlock(){
-	__sync_lock_release(&lock);
-}
-
-
-struct list_args {
-	int iterations;
-	SortedListElement_t *elems;
-	SortedList_t *list;
-	void (*lock_fun)();
-	void (*unlock_fun)();
-};
-
-void *iterative_list_ops(void *args){
-	struct list_args *l_args = (struct list_args*)args;
-
-	for (int i = 0; i < l_args->iterations; i++){
-		(*(l_args->lock_fun))();
-		SortedList_insert(l_args->list, &(l_args->elems[i]));
-		(*(l_args->unlock_fun))();
-	}
-
-	(*(l_args->lock_fun))();
-	SortedList_length(l_args->list);
-	(*(l_args->unlock_fun))();
-
-	for (int i = 0; i < l_args->iterations; i++){
-		(*(l_args->lock_fun))();
-		SortedListElement_t *found = SortedList_lookup(l_args->list, (l_args->elems[i]).key);
-		SortedList_delete(found);
-		(*(l_args->unlock_fun))();
-	}
-
-	free(l_args);
-	pthread_exit(NULL);
-}
-
 long elapsed_time(struct timespec *start, struct timespec *finish){
 	long sec_time = finish->tv_sec - start->tv_sec;
 	long nano_time = finish->tv_nsec - start->tv_nsec;
 	return (sec_time * 1000000000) + nano_time;
 }
 
+void no_op(){}
+
+void mutex_lock(pthread_mutex_t *mutex){
+	pthread_mutex_lock(mutex);
+}
+
+void mutex_unlock(pthread_mutex_t *mutex){
+	pthread_mutex_unlock(mutex);
+}
+
+void spin_lock(volatile int *spin_lock){
+	while(__sync_lock_test_and_set(spin_lock, 1));
+}
+
+void spin_unlock(volatile int *spin_lock){
+	__sync_lock_release(spin_lock);
+}
+
+
+struct list_args {
+	int iterations;
+	SortedListElement_t *elems;
+	SortedList_t *sub_list;
+	sync_option sync;
+	long lock_time;
+	listLocks *locks;
+};
+
+void *iterative_list_ops(void *args){
+	struct list_args *l_args = (struct list_args*)args;
+	struct timespec start, finish;
+
+	for (int i = 0; i < l_args->iterations; i++){
+		switch (l_args->sync){
+			case NONE:
+				SortedList_insert(l_args->sub_list, &(l_args->elems[i]));
+				break;
+			case MUTEX:
+				if (clock_gettime(CLOCK_MONOTONIC, &start) < 0) error("clock_gettime");
+
+				mutex_lock(&(l_args->locks->mutex));
+				SortedList_insert(l_args->sub_list, &(l_args->elems[i]));
+				mutex_unlock(&(l_args->locks->mutex));
+
+				if (clock_gettime(CLOCK_MONOTONIC, &finish) < 0) error("clock_gettime");
+				l_args->lock_time += elapsed_time(&start, &finish);
+
+				break;
+			case SPIN:
+				spin_lock(&(l_args->locks->spin_lock));
+				SortedList_insert(l_args->sub_list, &(l_args->elems[i]));
+				spin_unlock(&(l_args->locks->spin_lock));
+				break;
+			default:
+				SortedList_insert(l_args->sub_list, &(l_args->elems[i]));
+				break;
+		}
+	}
+
+	switch (l_args->sync){
+		case NONE:
+			SortedList_length(l_args->sub_list);
+			break;
+		case MUTEX:
+			if (clock_gettime(CLOCK_MONOTONIC, &start) < 0) error("clock_gettime");
+
+			mutex_lock(&(l_args->locks->mutex));
+			SortedList_length(l_args->sub_list);
+			mutex_unlock(&(l_args->locks->mutex));
+
+			if (clock_gettime(CLOCK_MONOTONIC, &finish) < 0) error("clock_gettime");
+			l_args->lock_time += elapsed_time(&start, &finish);
+
+			break;
+		case SPIN:
+			spin_lock(&(l_args->locks->spin_lock));
+			SortedList_length(l_args->sub_list);
+			spin_unlock(&(l_args->locks->spin_lock));
+			break;
+		default:
+			SortedList_length(l_args->sub_list);
+			break;
+	}
+
+	SortedListElement_t *found;
+	for (int i = 0; i < l_args->iterations; i++){
+		switch (l_args->sync){
+			case NONE:
+				found = SortedList_lookup(l_args->sub_list, (l_args->elems[i]).key);
+				SortedList_delete(found);
+				break;
+			case MUTEX:
+				if (clock_gettime(CLOCK_MONOTONIC, &start) < 0) error("clock_gettime");
+
+				mutex_lock(&(l_args->locks->mutex));
+				found = SortedList_lookup(l_args->sub_list, (l_args->elems[i]).key);
+				SortedList_delete(found);
+				mutex_unlock(&(l_args->locks->mutex));
+
+				if (clock_gettime(CLOCK_MONOTONIC, &finish) < 0) error("clock_gettime");
+				l_args->lock_time += elapsed_time(&start, &finish);
+				break;
+			case SPIN:
+				spin_lock(&(l_args->locks->spin_lock));
+				found = SortedList_lookup(l_args->sub_list, (l_args->elems[i]).key);
+				SortedList_delete(found);
+				spin_unlock(&(l_args->locks->spin_lock));
+				break;
+			default:
+				found = SortedList_lookup(l_args->sub_list, (l_args->elems[i]).key);
+				SortedList_delete(found);
+				break;
+		}
+	}
+
+	// free(l_args);
+	pthread_exit(NULL);
+}
+
 struct options_args {
 	int *threads_ptr;
 	int *iterations_ptr;
 	char syncOpt;
-	void (**lock_fun_ptr)();
-	void (**unlock_fun_ptr)();
+	sync_option *sync_ptr;
+	int *num_lists_ptr;
 };
 
 void parse_options(int argc, char *argv[], struct options_args *o_args){
@@ -91,10 +166,11 @@ void parse_options(int argc, char *argv[], struct options_args *o_args){
 		{"threads", required_argument, 0, 't'},
 		{"iterations", required_argument, 0, 'i'},
 		{"sync", required_argument, 0, 's'},
-		{"yield", required_argument, 0, 'y'}
+		{"yield", required_argument, 0, 'y'},
+		{"lists", required_argument, 0, 'l'}
 	};
 
-	while ((opt_char = getopt_long(argc, argv, "t:i:y:s:", long_options, NULL)) != -1){
+	while ((opt_char = getopt_long(argc, argv, "t:i:y:s:l:", long_options, NULL)) != -1){
 		switch (opt_char){
 			case 't':
 				*(o_args->threads_ptr) = atoi(optarg);
@@ -106,13 +182,10 @@ void parse_options(int argc, char *argv[], struct options_args *o_args){
 				o_args->syncOpt = *optarg;
 				switch (*optarg){
 					case 'm':
-						pthread_mutex_init(&mutex,NULL);
-						*(o_args->lock_fun_ptr) = &mutex_lock;
-						*(o_args->unlock_fun_ptr) = &mutex_unlock;
+						*(o_args->sync_ptr) = MUTEX;
 						break;
 					case 's':
-						*(o_args->lock_fun_ptr) = &spin_lock;
-						*(o_args->unlock_fun_ptr) = &spin_unlock;
+						*(o_args->sync_ptr) = SPIN;
 						break;
 					default:
 						break;
@@ -135,6 +208,9 @@ void parse_options(int argc, char *argv[], struct options_args *o_args){
 					}
 				}
 				break;
+			case 'l':
+				*(o_args->num_lists_ptr) = atoi(optarg);
+				break;
 			default:
 				break;
 		}
@@ -144,23 +220,21 @@ void parse_options(int argc, char *argv[], struct options_args *o_args){
 int main(int argc, char *argv[]){
 	int num_threads = 1;
 	int num_iterations = 1;
-	void (*lock_fun)() = &no_op;
-	void (*unlock_fun)() = &no_op;
+	sync_option sync = NONE;
+	int num_lists = 1;
 
 
 	struct options_args o_args;
 	o_args.threads_ptr = &num_threads;
 	o_args.iterations_ptr = &num_iterations;
-	o_args.lock_fun_ptr = &lock_fun;
-	o_args.unlock_fun_ptr = &unlock_fun;
+	o_args.sync_ptr = &sync;
+	o_args.num_lists_ptr = &num_lists;
 
 	parse_options(argc, argv, &o_args);
 	int num_ops = num_threads * num_iterations * 3;
 	// fprintf(stdout, "num_threads %d, num_iterations %d, num_ops %d, sync %c, opt_yield %d\n", 
 	// 	num_threads, num_iterations, num_ops, o_args.syncOpt, opt_yield);
 
-
-	//A stupid amount of work for just the test name
 	char test_name[50];
 	char *test_prefix = "list-";
 	char *none = "none";
@@ -210,10 +284,16 @@ int main(int argc, char *argv[]){
 	test_name[test_idx] = '\0';
 
 	//initializes an empty list.
-	SortedList_t *list = (SortedList_t *) malloc(sizeof(SortedList_t));
-	list->key = NULL;
-	list->prev = NULL;
-	list->next = NULL;
+	SortedList_t *lists = (SortedList_t *) malloc(num_lists * sizeof(SortedList_t));
+	listLocks locks_arr[num_lists];
+	for (int i = 0; i < num_lists; i++){
+		lists[i].key = NULL;
+		lists[i].prev = NULL;
+		lists[i].next = NULL;
+		pthread_mutex_init(&locks_arr[i].mutex, NULL);
+		locks_arr[i].spin_lock = 0;
+	}
+
 
 
 	char *charset = "abcdefghijklmnopqrstuvwxyz";
@@ -241,40 +321,49 @@ int main(int argc, char *argv[]){
 	struct timespec start, finish;
 	if (clock_gettime(CLOCK_MONOTONIC, &start) < 0) error("clock_gettime");
 
-	struct list_args *l_args;
+	struct list_args *l_args_arr = (struct list_args*) malloc(num_threads * sizeof(struct list_args));
 	for (int i = 0; i < num_threads; i++){
-		l_args = (struct list_args *) malloc(sizeof(struct list_args));
-		l_args->iterations = num_iterations;
-		l_args->elems = list_elements[i];
-		l_args->list = list;
-		l_args->lock_fun = lock_fun;
-		l_args->unlock_fun = unlock_fun;
-		if (pthread_create(&threads[i], NULL, iterative_list_ops, l_args) < 0){
+		l_args_arr[i].iterations = num_iterations;
+		l_args_arr[i].elems = list_elements[i];
+		l_args_arr[i].sub_list = &lists[i % num_lists];
+		l_args_arr[i].sync = sync;
+		l_args_arr[i].lock_time = 0;
+		l_args_arr[i].locks = &locks_arr[i % num_lists];
+		if (pthread_create(&threads[i], NULL, iterative_list_ops, &l_args_arr[i]) < 0){
 			error("pthread_create");
 		}
 	}
 
 	void *status;
+	long total_lock_time = 0;
 	for (int i = 0; i < num_threads; i++){
 		if (pthread_join(threads[i], &status) < 0){
 			error("pthread_join");
 		}
+		total_lock_time += l_args_arr[i].lock_time;
 	}
+	free(l_args_arr);
 
 	if (clock_gettime(CLOCK_MONOTONIC, &finish) < 0) error("clock_gettime");
 
-	int exit_code = 0;
-	if (SortedList_length(list) != 0){
+	int error = 0;
+	for (int i = 0; i < num_lists; i++){
+		if (SortedList_length(&lists[i]) != 0){
+			error = 1;
+			break;
+		}
+	}
+	
+	if (error == 1){
 		fprintf(stderr, "ERROR, list length is not 0\n");
-		exit_code = 1;
 	}
 	else {
 		long tot_run_time = elapsed_time(&start, &finish);
 		long avg_run_time = tot_run_time / num_ops;
-		fprintf(stdout, "%s,%d,%d,%d,%d,%lu,%lu\n",
-			test_name, num_threads, num_iterations, 1, num_ops, tot_run_time, avg_run_time);
+		long avg_lock_time = total_lock_time / num_ops;
+		fprintf(stdout, "%s,%d,%d,%d,%d,%d,%lu,%lu,%lu\n",
+			test_name, num_lists, num_threads, num_iterations, 1, num_ops, tot_run_time, avg_run_time, avg_lock_time);
 	}
-
 	
 	for (int i = 0; i < num_threads; i++){
 		for (int j = 0; j < num_iterations; j++){
@@ -283,7 +372,11 @@ int main(int argc, char *argv[]){
 		free(list_elements[i]);
 	}
 	free(list_elements);
-	free(list);
 
-	exit(exit_code);
+	for (int i = 0; i < num_lists; i++){
+		pthread_mutex_destroy(&(locks_arr[i].mutex));
+	}
+	free(lists);
+
+	exit(0);
 }
--- lab2b/lab2b.gp
+++ lab2b/lab2b.gp
@@ -14,19 +14,22 @@
 #
 # input: lab2_list.csv
 #	1. test name
-#	2. # threads
-#	3. # iterations per thread
-#	4. # lists
-#	5. # operations performed (threads x iterations x (ins + lookup + delete))
-#	6. run time (ns)
-#	7. run time per operation (ns)
+#   2. number of lists
+#	3. # threads
+#	4. # iterations per thread
+#	5. # lists
+#	6. # operations performed (threads x iterations x (ins + lookup + delete))
+#	7. run time (ns)
+#	8. run time per operation (ns)
+#   9. Avg wait for lock time (only mutex)
+#   
 #
 # output:
-#	lab2b_1.png ... threads and iterations that run (unprotected) w/o failure
-#	lab2_add-2.png ... cost per operation of yielding
-#	lab2_add-3.png ... cost per operation vs number of iterations
-#	lab2_add-4.png ... threads and iterations that run (protected) w/o failure
-#	lab2_add-5.png ... cost per operation vs number of threads
+#	lab2b_1.png ... add and list: throughput vs number of threads
+#	lab2b_2.png ... mutexes: wait for lock time and avg operation time vs num threads 
+#	lab2b_3.png ... cost per operation vs number of iterations
+#	lab2b_4.png ... threads and iterations that run (protected) w/o failure
+#	lab2b_5.png ... cost per operation vs number of threads
 #
 # Note:
 #	Managing data is simplified by keeping all of the results in a single
@@ -38,17 +41,77 @@
 set terminal png
 set datafile separator ","
 
-set title "Add: throughput vs number of threads"
-set xlabel "Threads"
+set title "Throughput vs Number of Threads"
+set xlabel "Num Threads"
 set logscale x 2
 set xrange [0.75:]
-set ylabel "throughput ops/s"
+set ylabel "Throughput (ops/s)"
 set logscale y 10
 set output 'lab2b_1.png'
 set key right top
 # grep out only successful (sum=0) yield runs
 plot \
-     "< grep 'add-m' lab2_add.csv" using ($2):(1000000000/$6) title 'add mutex' with linespoints lc rgb 'red', \
-     "< grep 'add-s' lab2_add.csv" using ($2):(1000000000/$6) title 'add spin' with linespoints lc rgb 'green', \
-     "< grep 'list-none-m' lab2_list.csv" using ($2):(1000000000/$7) title 'list mutex' with linespoints lc rgb 'blue', \
-     "< grep 'list-none-s' lab2_list.csv" using ($2):(1000000000/$7) title 'list spin' with linespoints lc rgb 'orange'
+     "< grep 'add-m,.*,10000,' lab2_add.csv" using ($2):(1000000000/$6) title 'add mutex' with linespoints lc rgb 'red', \
+     "< grep 'add-s,.*,10000,' lab2_add.csv" using ($2):(1000000000/$6) title 'add spin' with linespoints lc rgb 'green', \
+     "< grep 'list-none-m,1,.*,1000,' lab2_list.csv" using ($3):(1000000000/$8) title 'list mutex' with linespoints lc rgb 'blue', \
+     "< grep 'list-none-s,1,.*,1000,' lab2_list.csv" using ($3):(1000000000/$8) title 'list spin' with linespoints lc rgb 'orange'
+
+
+set title "Mutex: Avg Wait for Lock Time, Avg Time Per Operation vs Number of Threads"
+set xlabel "Num Threads"
+set logscale x 2
+set xrange [0.75:]
+set ylabel "Avg Time/Op (s)"
+set logscale y 10
+set output 'lab2b_2.png'
+set key right top
+# grep out only successful (sum=0) yield runs
+plot \
+     "< grep 'list-none-m,1,.*,1000' lab2_list.csv" using ($3):($8) title 'time per operation' with linespoints lc rgb 'red', \
+     "< grep 'list-none-m,1,.*,1000' lab2_list.csv" using ($3):($9) title 'wait for lock' with linespoints lc rgb 'blue'
+
+
+set title "Lists: Unprotected Threads and Iterations that run without failure"
+set xlabel "Num Threads"
+set logscale x 2
+set xrange [0.75:]
+set ylabel "Successful Iterations"
+set logscale y 10
+set output 'lab2b_3.png'
+# note that unsuccessful runs should have produced no output
+plot \
+     "< grep list-id-none,4, lab2_list.csv" using ($3):($4) title 'unprotected' with points lc rgb 'red', \
+     "< grep list-id-m,4, lab2_list.csv" using ($3):($4) title 'mutex' with points lc rgb 'green', \
+     "< grep list-id-s,4, lab2_list.csv" using ($3):($4) title 'spin' with points lc rgb 'blue'
+
+
+set title "Multiple Lists, Mutex: Throughput vs Lists"
+set xlabel "Num Threads"
+set logscale x 2
+set xrange [0.75:]
+set ylabel "Throughput (ops/s)"
+set logscale y 10
+set output 'lab2b_4.png'
+set key right top
+# grep out only successful (sum=0) yield runs
+plot \
+     "< grep 'list-none-m,1,.*,1000,' lab2_list.csv" using ($3):(1000000000/$8) title '1 lists' with linespoints lc rgb 'red', \
+     "< grep 'list-none-m,4,.*,1000,' lab2_list.csv" using ($3):(1000000000/$8) title '4 lists' with linespoints lc rgb 'green', \
+     "< grep 'list-none-m,8,.*,1000,' lab2_list.csv" using ($3):(1000000000/$8) title '8 lists' with linespoints lc rgb 'violet', \
+     "< grep 'list-none-m,16,.*,1000,' lab2_list.csv" using ($3):(1000000000/$8) title '16 lists' with linespoints lc rgb 'blue'
+
+
+set title "Multiple Lists, Spin Lock: Throughput vs Lists"
+set xlabel "Num Threads"
+set logscale x 2
+set xrange [0.75:]
+set ylabel "Throughput (ops/s)"
+set logscale y 10
+set output 'lab2b_5.png'
+set key right top
+# grep out only successful (sum=0) yield runs
+plot \
+     "< grep 'list-none-s,1,.*,1000,' lab2_list.csv" using ($3):(1000000000/$8) title '1 lists' with linespoints lc rgb 'red', \
+     "< grep 'list-none-s,4,.*,1000,' lab2_list.csv" using ($3):(1000000000/$8) title '4 lists' with linespoints lc rgb 'green', \
+      "< grep 'list-none-s,8,.*,1000,' lab2_list.csv" using ($3):(1000000000/$8) title '8 lists' with linespoints lc rgb 'violet', \
+     "< grep 'list-none-s,16,.*,1000,' lab2_list.csv" using ($3):(1000000000/$8) title '16 lists' with linespoints lc rgb 'blue'
